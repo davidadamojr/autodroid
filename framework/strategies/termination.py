1488c52767edd97da7a7b1da52556777f15027d8
David Adamo Jr <me@davidadamojr.com>
2018-05-17 21:21:15 -0400

    fixed database issue

--- framework/strategies/termination.py
+++ framework/strategies/termination.py
@@ -1,28 +1,27 @@
 import logging
 import random
-from  framework.database import test_case_exists
 
 
-def probabilistic(db_connection, probability=None, test_case_hash=None, test_suite_id=None, event_count=None,):
+def probabilistic(database, probability=None, test_case_hash=None, test_suite_id=None, event_count=None,):
     assert probability is not None and test_suite_id is not None and test_case_hash is not None
 
     logger = logging.getLogger(__name__)
     logger.info("Checking probabilistic test termination criterion...")
 
-    if random.random() < probability and test_case_exists(db_connection, test_suite_id, test_case_hash):
+    if random.random() < probability and database.test_case_exists(test_suite_id, test_case_hash):
         return True
 
     return False
 
 
-def length(db_connection, test_case_length=None, test_case_hash=None, test_suite_id=None, event_count=None):
+def length(database, test_case_length=None, test_case_hash=None, test_suite_id=None, event_count=None):
     assert event_count is not None and test_case_length is not None and test_suite_id is not None and \
         test_suite_id is not None
 
     logger = logging.getLogger(__name__)
     logger.info("Checking length test termination criterion...")
 
-    if event_count >= test_case_length and not test_case_exists(db_connection, test_suite_id, test_case_hash):
+    if event_count >= test_case_length and not database.test_case_exists(test_suite_id, test_case_hash):
         return True
 
     return False
--- tests/integration/script_tests.py
+++ tests/integration/script_tests.py
@@ -4,9 +4,9 @@ import config
 from framework.utils.scripts import *
 
 
-"""These tests require a running emulator and the Moneybalance app"""
+"""These tests require a running emulator and the Tomdroid app"""
 
-APP_PACKAGE = "ivl.android.moneybalance"
+APP_PACKAGE = "org.tomdroid"
 
 class ScriptTests(unittest.TestCase):
 
